ccc6846a1b7c6d5baf7cc574d9dbb53cf8148ddf
VRMOOIN <andls4567@naver.com>
2024-12-15 21:47:21 +0900

    Master Branch에서 A파일 수정 하여 새로운 버전 생성

--- src/A.java
+++ src/A.java
@@ -1,5 +1,5 @@
 public class A {
     public static void main(String[] args) {
-        System.out.println("AAAAAAAAAAAAAAAA");
+        System.out.println("It's for master branch - updated version");
     }
 }
